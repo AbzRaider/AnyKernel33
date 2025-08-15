379448290ccc2c9ff7b3f795235ac3620886e0b7
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-08-15 13:50:29 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,8 +4,8 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=LineagePlus Kernel for Xiaomi MT6893 Series
-do.devicecheck=1
+kernel.string=LineagePlus Kernel for Realme MT6785 Series
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
